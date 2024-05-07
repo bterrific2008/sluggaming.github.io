020477573d49eb46bc208a1bc4fb01a94d6b38dc
Jeffuz <jeffzhang0049@gmail.com>
2024-05-06 23:31:57 -0700

    update overwatch member details

--- src/pages/games/Overwatch.js
+++ src/pages/games/Overwatch.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import TeamCard from '../../components/Teams Page/TeamCard';
 import overwatchTeamData from '../../data/Overwatch Team.json';
+import overwatchTeamBData from '../../data/Overwatch Team B.json';
+
 
 const Overwatch = () => {
   return (
@@ -35,6 +37,24 @@ const Overwatch = () => {
           <div className='container mx-auto pb-24 pt-12 px-8 '>
             <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6'>
               {overwatchTeamData.map((teamMember) => (
+                <TeamCard key={teamMember.id} {...teamMember} />
+              ))}
+            </div>
+          </div>
+        </section>
+        <section className='font-Montserrat'>
+          <div className='flex justify-center sm:text-5xl text-3xl text-[#00588F]'>
+            UCSC B Team
+          </div>
+          <div className='flex justify-center max-[1100px]:px-[50px] pt-3'>
+            <img src={require('../../images/Achivements Page/Yellow Line.png')} alt='Yellow Line' loading="lazy" />
+          </div>
+          <div className='min-[1100px]:px-[215px] px-[50px] text-[25px] pt-[30px] text-center'>
+            University of California, Santa Cruz | Overwatch Junior Varsity Team
+          </div>
+          <div className='container mx-auto pb-24 pt-12 px-8 '>
+            <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6'>
+              {overwatchTeamBData.map((teamMember) => (
                 <TeamCard key={teamMember.id} {...teamMember} />
               ))}
             </div>
